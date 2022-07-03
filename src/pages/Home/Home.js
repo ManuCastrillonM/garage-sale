3158a642429919c9926cade7bbb79d54288ae24f
Manu Castrillon <manu.cm43@gmail.com>
2022-07-03 00:25:47 -0500

    Create pages folder
    Co-authored-by: svargaslondono <svargaslondono@gmail.com>

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,25 @@
+import logo from '../../logo.svg';
+import './Home.scss';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <img src={logo} className="Home-logo" alt="logo" />
+        <p>
+          Edit <code>src/Home.js</code> and save to reload.
+        </p>
+        <a
+          className="Home-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Home;

381c004bb43a790af1d9ba526782f062a538eba8
Manu Castrillon <manu.cm43@gmail.com>
2022-07-11 21:22:48 -0500

    Delete react home default component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,3 @@
-import logo from '../../logo.svg';
 import './Home.scss';
 import About from '../../components/About/About';
 import Footer from '../../components/Footer/Footer';
@@ -12,24 +11,6 @@ function home() {
       <div className="home__content">
         <Header />
         <About />
-        <header className="home-header">
-          <img src={logo} className="home-logo" alt="logo" />
-          <p>
-            Edit
-            {' '}
-            <code>src/home.js</code>
-            {' '}
-            and save to reload.
-          </p>
-          <a
-            className="home-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
       <Footer />
     </div>

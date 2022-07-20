a451fc8c56ca81e015cf336b97cdc03e945aee73
Manu Castrillon <manu.cm43@gmail.com>
2022-07-20 15:02:08 -0500

    Update color palette.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,12 +2,10 @@ import './Home.scss';
 import About from '../../components/About/About';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
-import Navigation from '../../components/Navigation/Navigation';
 
 function home() {
   return (
     <div className="home">
-      <Navigation />
       <div className="home__content">
         <Header />
         <About />

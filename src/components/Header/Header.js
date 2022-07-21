6f413bdf69b8b3244bd21abc0b9dac1a7c383b73
Manu Castrillon <manu.cm43@gmail.com>
2022-07-20 19:21:11 -0500

    update header styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,42 @@
 import './Header.scss';
+import headerImg from '../../assets/images/header-image.png';
 
 function Header() {
+  const animateHeader = (e) => {
+    const element = e.target;
+
+    if (e.type === 'mouseenter') {
+      const { clientX, clientY } = e;
+      const {
+        clientHeight, clientWidth, offsetLeft, offsetTop,
+      } = element;
+
+      const imageCenterX = offsetLeft + (clientWidth / 2);
+      const imageCenterY = offsetTop + (clientHeight / 2);
+
+      const newClientX = clientX >= imageCenterX ? -5 : 5;
+      const newClientY = clientY >= imageCenterY ? -5 : 5;
+
+      element.style.transform = `translate(${newClientX}px, ${newClientY}px)`;
+    } else {
+      element.style.transform = 'translate(0)';
+    }
+  };
+
   return (
     <header className="header">
       <div className="header__container">
-        <h1 className="header__title">El garaje de Mango</h1>
+        <h1 className="header__title">
+          La tiendita
+          {' '}
+          <br />
+          {' '}
+          de Mango
+        </h1>
+        <a className="header__cta" href="/">Ver productos</a>
+      </div>
+      <div className="header__image-container">
+        <img className="header__image" onMouseEnter={(e) => animateHeader(e)} onMouseLeave={(e) => animateHeader(e)} src={headerImg} alt="Mango" styles="" />
       </div>
     </header>
   );

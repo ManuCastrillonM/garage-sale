6b84b48fe890efd6437e68b9b93e9efcdf91cef4
manuCastrillonM <manu.cm43@gmail.com>
2022-09-17 18:57:43 -0500

    Add stylelint configuration

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 
-import './Home.scss';
 import About from '../../components/About/About';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';

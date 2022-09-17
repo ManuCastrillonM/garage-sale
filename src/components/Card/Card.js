e78f4d2a7eaa94625caf837e7f1d644997e0202c
Sebastian Vargas Londoño <svargaslondono@gmail.com>
2022-09-17 17:23:25 -0500

    Update Card component with new model (#3)
    Co-authored-by: Sebastian Vargas <svargaslondono@Sebastians-MacBook-Pro.local>

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types';
 import './Card.scss';
 
 function Card({
-  name, category, coverImageIndex, description, images,
+  name, categories, coverImageIndex, description, images,
 }) {
-  const coverImage = images[coverImageIndex]?.fields?.file?.url || null;
+  const coverImage = (images && images[coverImageIndex].fields?.file?.url) || null;
   const whatsAppUrl = `${process.env.REACT_APP_WHATSAPP_URL}?text=Hola!%20Me%20interesa%20un%20producto%20que%20tienes%20a%20la%20venta:%20${name}`;
   return (
     <div className="card">
@@ -14,9 +14,14 @@ function Card({
         <div className="card__image card__image--empty">{name}</div>
       )}
       <div className="card__content">
-        <span className="card__category">{category}</span>
+        <span className="card__category">
+          {categories.map((category, index) => ((index > 0) ? ', ' : '') + category)}
+        </span>
         <span className="card__name">{name}</span>
-        <span className="card__description">{description}</span>
+        {
+          description
+          && <span className="card__description">{description}</span>
+        }
         <a
           className="card__wa"
           href={whatsAppUrl}
@@ -31,14 +36,16 @@ function Card({
 }
 Card.propTypes = {
   name: PropTypes.string.isRequired,
-  category: PropTypes.string.isRequired,
+  categories: PropTypes.arrayOf(PropTypes.string).isRequired,
   coverImageIndex: PropTypes.number,
-  description: PropTypes.string.isRequired,
-  images: PropTypes.any.isRequired,
+  description: PropTypes.string,
+  images: PropTypes.any,
 };
 
 Card.defaultProps = {
   coverImageIndex: 0,
+  description: null,
+  images: null,
 };
 
 export default Card;

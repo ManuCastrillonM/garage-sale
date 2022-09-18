31f344aad16963c02b54dcdec08d6b455dd673f0
Sebastian Vargas <svargaslondono@Sebastians-MacBook-Pro.local>
2022-09-18 00:30:33 -0500

    Products cta leads to section

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -4,7 +4,7 @@ import './CardList.scss';
 
 function CardList({ items }) {
   return (
-    <section className="card-list">
+    <section id="products" className="card-list">
       {items.map((item) => (
         <Card {...item.fields} key={item.sys.id} />
       ))}

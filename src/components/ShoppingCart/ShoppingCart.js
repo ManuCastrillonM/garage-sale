ec113686133a855c760c21b6ed1cd489d68c05ca
manuCastrillonM <manu.cm43@gmail.com>
2022-10-02 22:25:22 -0500

    fix shopping cart styles on mobile

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -5,42 +5,42 @@ import whatsappIcon from '../../assets/icons/whatsapp.svg';
 function ShoppingCart({ handleClose }) {
   const whatsAppUrl = `${process.env.REACT_APP_WHATSAPP_URL}?text=Hola!%20Me%20interesan%20estos%20productos%20que%20tienes%20a%20la%20venta:%20`;
 
-  const onClose = () => {
-    handleClose();
-  };
+  const shoppingItems = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
 
   return (
     <>
-      <div className="shopping-cart__overlay" onClick={onClose} role="presentation" />
+      <div className="shopping-cart__overlay" onClick={() => handleClose()} role="presentation" />
       <div className="shopping-cart">
-        <button type="button" className="shopping-cart__close" onClick={onClose} data-testid="close-shopping-cart-btn"><span className="hidden">close</span></button>
-        <h1 className="shopping-cart__title">Productos que te interesan</h1>
-        <ul>
-          <li>
-            <div className="shopping-cart__item">
-              lampara de noche
+        <div className="shopping-cart__header">
+          <button
+            type="button"
+            className="shopping-cart__close"
+            onClick={() => handleClose()}
+            data-testid="close-shopping-cart-btn"
+          >
+            <span className="hidden">close</span>
+          </button>
+          <h1 className="shopping-cart__title">Productos que te interesan</h1>
+        </div>
+
+        {shoppingItems.length > 0 ? (
+          <>
+            <div className="shopping-cart__content">
+              <ul>
+                {shoppingItems.map((item) => (
+                  <li className="shopping-cart__item" key={item}>{item}</li>
+                ))}
+              </ul>
             </div>
-          </li>
-          <li>
-            <div className="shopping-cart__item">
-              máquina de coser
+            <div className="shopping-cart__footer">
+              <a href={whatsAppUrl} className="shopping-cart__button" target="_blank" rel="noreferrer">
+                <img src={whatsappIcon} alt="logo de whatsapp" />
+                Contacto
+              </a>
             </div>
-          </li>
-          <li>
-            <div className="shopping-cart__item">
-              caja de herramientas
-            </div>
-          </li>
-          <li>
-            <div className="shopping-cart__item">
-              bicicleta
-            </div>
-          </li>
-        </ul>
-        <a href={whatsAppUrl} className="shopping-cart__button" target="_blank" rel="noreferrer">
-          <img src={whatsappIcon} alt="logo de whatsapp" />
-          Contacto
-        </a>
+          </>
+        ) : <p>No tienes elementos en tu carrito de compras</p>}
+
       </div>
     </>
   );

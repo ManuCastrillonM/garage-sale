78f7f1d092b4989d4d55e8512f7c9011362f8217
manuCastrillonM <manu.cm43@gmail.com>
2022-10-07 22:27:48 -0500

    Show all the elements in the carousel and hide gallery buttons when necessary

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,16 +1,27 @@
 import PropTypes from 'prop-types';
-import { useRef } from 'react';
+import { useMemo, useRef, useState } from 'react';
 import './ImageGallery.scss';
 
 function ImageGallery({ images, name }) {
   const container = useRef(null);
 
+  const [currentIndex, setCurrentIndex] = useState(1);
+
+  const isFirstImage = useMemo(() => currentIndex === 1, [currentIndex]);
+  const isLastImage = useMemo(() => currentIndex === images.length, [currentIndex]);
+
   const moveScroll = (multiplier) => {
+    const imageSize = container.current.firstChild.clientWidth;
+    const newScrollPosition = container.current.scrollLeft + (imageSize * multiplier);
+
     container.current.scroll({
-      left: multiplier * container.current.firstChild.clientWidth,
+      left: newScrollPosition,
       behavior: 'smooth',
     });
+
+    multiplier === 1 ? setCurrentIndex(currentIndex + 1) : setCurrentIndex(currentIndex - 1);
   };
+
   return (
     <div className="gallery" data-testid="gallery">
       <div className="gallery__content" ref={container}>
@@ -18,8 +29,8 @@ function ImageGallery({ images, name }) {
           <img className="gallery__image" key={image} src={image} alt={`Imagen de ${name}`} />
         ))}
       </div>
-      <button className="gallery__previous" type="button" onClick={() => moveScroll(-1)} aria-label="Anterior imagen">&#10094;</button>
-      <button className="gallery__next" type="button" onClick={() => moveScroll(1)} aria-label="Siguiente imagen">&#10095;</button>
+      { !isFirstImage && <button className="gallery__previous" type="button" onClick={() => moveScroll(-1)} aria-label="Anterior imagen">&#10094;</button> }
+      { !isLastImage && <button className="gallery__next" type="button" onClick={() => moveScroll(1)} aria-label="Siguiente imagen">&#10095;</button> }
     </div>
   );
 }
